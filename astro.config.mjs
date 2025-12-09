92a4597695a77b2fb2c1f0dcd3cb8e0616f40d2d
Radiqalo <Radiqalo>
2025-12-10 00:21:26 +0800

    更新 Astro 配置，修正站点和基础路径；添加 GitHub Pages 部署工作流

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-        site: 'radiqalo.github.io',
-        base: '/docs'
+        site: 'https://radiqalo.github.io',
+        base: '/my-site'
 });
